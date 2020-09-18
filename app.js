767e2e7595c26e9b38ef03a8af9e7c566f9e905f
sym28 <29101705+sym28@users.noreply.github.com>
2020-09-18 14:02:25 +0100

    changes to index

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 
 const nanodegreeCard = document.getElementsByClassName("card");
+
+const mainHeading = document.querySelector('.mainHeading');
+const otherHeading = document.querySelector('.otherHeading');
+
+
+const someText = '!!!';
+
+mainHeading.insertAdjacentHTML("afterbegin", someText);
